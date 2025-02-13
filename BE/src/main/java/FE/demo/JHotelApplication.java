1131249cdb22aefc2bc061a2c068550216b533f0
HuyDau <lehuydau2312@gmail.com>
2025-02-13 13:53:33 +0700

    Create New Project FE + BE

---
+++ BE/src/main/java/FE/demo/JHotelApplication.java
@@ -0,0 +1,13 @@
+package FE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JHotelApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/FE/demo/JHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package FE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
